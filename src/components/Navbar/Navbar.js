2af57cae24ed53e3fe616d34d9613e4a2c4b1d18
Fio-Alanis <101599973+Fio-Alanis@users.noreply.github.com>
2022-07-17 18:07:21 -0300

    Add files via upload

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+                <div className='RFD'>
+                    RFD
+                </div>
+            <ul class='menu'>
+                <button className='boton'>Inicio</button>
+                <button className='boton'>Diseños</button>
+                <button className='boton'>Contacto</button>
+              </ul>
+        </nav>
+    )
+}
+
+export default Navbar
